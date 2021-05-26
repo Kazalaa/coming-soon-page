916e1fe68623549d2c8a427807989c48099acf50
Kazalaa <83344354+Kazalaa@users.noreply.github.com>
2021-05-26 16:12:20 +0200

    Update

---
+++ update.sh
@@ -0,0 +1,10 @@
+echo"COMMAND : git status"
+git status
+echo"COMMAND : git add --all"
+git add --all
+echo"COMMAND : git status"
+git status
+echo"COMMAND : git commit --m \"Update\""
+git commit --m "Update"
+echo"COMMAND : git push -u origin main"
+git push -u origin main
